d031cff5af7f194a0aaad16d3b0e9cff3b2204b7
1103409364 <1103409364@qq.com>
2020-11-30 22:13:48 +0800

    validate

--- app/routes/v1/classic.js
+++ app/routes/v1/classic.js
@@ -13,18 +13,19 @@ router.post('/v1/:id/classic/latest', (ctx, next) => {
   const query = ctx.request.query;
   const header = ctx.request.header;
   const body = ctx.request.body;
+
   // if (!query.c) {
   //   const error = new global.errors.ParameterException('错误原因:缺少参数', 10001);
   //   throw error;
   // }
   // ctx.body = { params: params };
   const schema = Joi.object({
-    username: Joi.string().alphanum().min(3).max(30).required(),
+    username: Joi.string().alphanum().min(3).max(10).required(),
     password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
-  })
-    .with('username', 'birth_year')
-    .xor('password', 'access_token')
-    .with('password', 'repeat_password');
+  });
+  // .with('username', 'birth_1year')
+  // .xor('password', 'access_token')
+  // .with('password', 'repeat_password');
 
   const { error, value } = schema.validate(body);
   if (error) {
@@ -32,10 +33,10 @@ router.post('/v1/:id/classic/latest', (ctx, next) => {
     const err = new global.errors.ParameterException(details[0].message, 10001);
     throw err;
   }
-  // ctx.body = {
-  //   // value,
-  //   ...error,
-  // };
+
+  ctx.body = {
+    username: value.username,
+  };
   // -> { value: { username: 'abc', birth_year: 1994 } }
   // -> { value: {}, error: '"username" is required' }
   // Also -
